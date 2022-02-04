36688902607a2eb9e8413dcb917e29ea0ad08ad8
enti <enti@enti.enti>
2022-02-04 11:17:44 +0100

    Introducing a connection to MongoDB

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 #!/usr/bin/node
 
-
 let http = require("http");
 
+let mongo_client = require("mongodb").MongoClient;
+
+let url = "mongodb://localhost/";
+
+console.log("Iniciando script mongo-http");
+
+mongo_client.connect(url, function(error, connection){
+
+	console.log("Dentro de MongoDB.");
+	console.log(error);
+	
+	if(error){
+		console.log("ERROR!!!");
+		return;
+	}
+
+	let database = connection.db("tffhd");
+
+	let characters  = database.collection("characters").find();
+
+	console.log(characters);
+
+});
+
+/*
 http.createServer(function(request, response){
 	response.writeHead(200);
 
@@ -11,3 +35,6 @@ http.createServer(function(request, response){
 	response.end(saludo);
 
 }).listen(1095);
+*/
+
+
