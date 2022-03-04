2062f226d2a876f7420b394446866e19fe1889f8
enti <enti@enti.enti>
2022-03-04 11:13:59 +0100

    añadir contenido de forma dinámica a través del acceso a una base de datos

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 
 let http = require("http");
+let fs = require("fs");
 
 let mongo_client = require("mongodb").MongoClient;
 
@@ -8,33 +9,55 @@ let url = "mongodb://localhost/";
 
 console.log("Iniciando script mongo-http");
 
+let database;
+
 mongo_client.connect(url, function(error, connection){
 
 	console.log("Dentro de MongoDB.");
 	console.log(error);
-	
+
 	if(error){
 		console.log("ERROR!!!");
 		return;
 	}
 
-	let database = connection.db("tffhd");
-
-	let characters  = database.collection("characters").find();
-
-	console.log(characters);
+	database = connection.db("tffhd");
 
 });
 
-/*
 http.createServer(function(request, response){
 	response.writeHead(200);
 
 	let saludo = "<h1>Muy buenos dias companero!</h1>";
 
-	response.end(saludo);
+	let array = [];
+
+	let i = 0;
+
+	let dataToSend;
+
+	var peticion;
+
+	peticion = request.url.substring(1);
+	
+	if(peticion == "index.html"){
+		fs.readFile("index.html", function (err, data){
+			response.writeHead(200, {"Content-Type" : "text/html"});
+			response.end(data);
+		})
+		return;
+	}
+
+	if(peticion.length == 0){peticion = " ";}	
+	let testing  = database.collection(peticion).find();
+	testing.toArray(function(error,data){
+		if(data.length != 0){
+			dataToSend  = JSON.stringify(data);
+			response.end(dataToSend);
+			}
+			else{
+			response.end("DATA NOT FOUND");
+			}
+		})
 
 }).listen(1095);
-*/
-
-
