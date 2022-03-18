aa86861d644e1bd6dd56bdf64c8d7c4c1bbb83d2
enti <enti@enti.enti>
2022-03-18 11:22:36 +0100

    Buscar elementos de una colección por id

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let http = require("http");
 let fs = require("fs");
 
 let mongo_client = require("mongodb").MongoClient;
+let ObjectId = require("mongodb").ObjectID;
 
 let url = "mongodb://localhost/";
 
@@ -25,6 +26,26 @@ mongo_client.connect(url, function(error, connection){
 
 });
 
+function SendDataList(database, response, peticion){
+	
+	console.log("trying to access characters");
+	let testing  = database.collection(peticion).find({}, { projection: { name: 1} });
+	testing.toArray(function(error,data){
+		if(data.length != 0){
+			dataToSend  = JSON.stringify(data);
+			response.end(dataToSend);
+			return;
+			}
+			else{
+			response.end("DATA NOT FOUND");
+			return;
+			}
+		})
+
+}
+
+
+
 http.createServer(function(request, response){
 	response.writeHead(200);
 
@@ -36,28 +57,37 @@ http.createServer(function(request, response){
 
 	let dataToSend;
 
-	var peticion;
-
-	peticion = request.url.substring(1);
+	var peticion = request.url;
 	
-	if(peticion == "index.html"){
+	if(peticion == "/"){
 		fs.readFile("index.html", function (err, data){
 			response.writeHead(200, {"Content-Type" : "text/html"});
 			response.end(data);
+			return;
 		})
 		return;
 	}
 
-	if(peticion.length == 0){peticion = " ";}	
-	let testing  = database.collection(peticion).find();
-	testing.toArray(function(error,data){
-		if(data.length != 0){
-			dataToSend  = JSON.stringify(data);
-			response.end(dataToSend);
-			}
-			else{
-			response.end("DATA NOT FOUND");
-			}
-		})
+	peticion = request.url.split("/");
+	
+	if(peticion.length == 2){
+		SendDataList(database, response, peticion[1]);
+	}
+	else
+	{
+		if(peticion[2].length != 24){ 
+			response.end();
+			return ;
+		}
 
+		if(peticion[1] == "characters"){
+			
+			let obj_id = new ObjectId(peticion[2]);
+			let colData = database.collection("characters").find({"_id":obj_id}, {projection:{ name:1 } });
+			colData.toArray(function(err, data){
+				let string = JSON.stringify(data);
+				response.end(string);
+			});
+		}
+	}
 }).listen(1095);
